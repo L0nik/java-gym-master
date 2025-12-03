2ac91b35ea20e44977ffa3a17147df9bb48c59bf
l0nik <l0nik@mail.ru>
2025-12-03 22:36:40 +0300

    getCountByCoaches() method

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Timetable {
 
-    private HashMap<DayOfWeek, TreeMap<TimeOfDay, ArrayList<TrainingSession>>> timetable;
+    private HashMap<DayOfWeek, TreeMap<TimeOfDay, ArrayList<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
         DayOfWeek day = trainingSession.getDayOfWeek();
@@ -33,5 +33,29 @@ public class Timetable {
     public ArrayList<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         TreeMap<TimeOfDay, ArrayList<TrainingSession>> trainingSessionsForDay = timetable.get(dayOfWeek);
         return trainingSessionsForDay.get(timeOfDay);
+    }
+
+    public ArrayList<CounterOfTrainings> getCountByCoaches() {
+        HashMap<Coach, Integer> counter = new HashMap<>();
+        for (TreeMap<TimeOfDay, ArrayList<TrainingSession>> dayData : this.timetable.values()) {
+            for (ArrayList<TrainingSession> sessionsList : dayData.values()) {
+                for (TrainingSession session : sessionsList) {
+                    if (counter.containsKey(session.getCoach())) {
+                        counter.put(session.getCoach(), counter.get(session.getCoach()) + 1);
+                    } else {
+                        counter.put(session.getCoach(), 1);
+                    }
+                }
+            }
+        }
+
+        ArrayList<CounterOfTrainings> result = new ArrayList<>();
+        for (Map.Entry<Coach, Integer> entry : counter.entrySet()) {
+            result.add(new CounterOfTrainings(entry.getKey(), entry.getValue()));
+        }
+
+        Collections.sort(result);
+
+        return result;
     }
 }
--- test/ru/yandex/practicum/gym/TimetableTest.java
+++ test/ru/yandex/practicum/gym/TimetableTest.java
@@ -19,7 +19,13 @@ public class TimetableTest {
         timetable.addNewTrainingSession(singleTrainingSession);
 
         //Проверить, что за понедельник вернулось одно занятие
+        TreeMap<TimeOfDay, ArrayList<TrainingSession>> trainingSessionsForMonday = timetable.getTrainingSessionsForDay(DayOfWeek.MONDAY);
+        Assertions.assertEquals(1, trainingSessionsForMonday.size());
+        ArrayList<TrainingSession> trainingSessionsForTime = trainingSessionsForMonday.get(new TimeOfDay(13, 0));
+        Assertions.assertEquals(1, trainingSessionsForTime.size());
+
         //Проверить, что за вторник не вернулось занятий
+        Assertions.assertNull(timetable.getTrainingSessionsForDay(DayOfWeek.TUESDAY));
     }
 
     @Test
@@ -47,8 +53,38 @@ public class TimetableTest {
         timetable.addNewTrainingSession(saturdayChildTrainingSession);
 
         // Проверить, что за понедельник вернулось одно занятие
+        Assertions.assertEquals(1, timetable.getTrainingSessionsForDay(DayOfWeek.MONDAY).size());
+
         // Проверить, что за четверг вернулось два занятия в правильном порядке: сначала в 13:00, потом в 20:00
+        Assertions.assertEquals(2, timetable.getTrainingSessionsForDay(DayOfWeek.THURSDAY).size());
+        TimeOfDay[] timeOfDayArray = new TimeOfDay[2];
+        int i = 0;
+        for (TimeOfDay timeOfDay : timetable.getTrainingSessionsForDay(DayOfWeek.THURSDAY).navigableKeySet()) {
+            timeOfDayArray[i] = timeOfDay;
+            i++;
+        }
+        Assertions.assertEquals(new TimeOfDay(13, 0), timeOfDayArray[0]);
+        Assertions.assertEquals(new TimeOfDay(20, 0), timeOfDayArray[1]);
+
         // Проверить, что за вторник не вернулось занятий
+        Assertions.assertNull(timetable.getTrainingSessionsForDay(DayOfWeek.TUESDAY));
+    }
+
+    @Test
+    void testGetTrainingSessionsForDayMultipleSessionsAtTheSameTime() {
+        Timetable timetable = new Timetable();
+
+        Group group = new Group("Акробатика для детей", Age.CHILD, 60);
+        Coach coach = new Coach("Васильев", "Николай", "Сергеевич");
+        TrainingSession trainingSession1 = new TrainingSession(group, coach,
+                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+        TrainingSession trainingSession2 = new TrainingSession(group, coach,
+                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+
+        timetable.addNewTrainingSession(trainingSession1);
+        timetable.addNewTrainingSession(trainingSession2);
+
+        Assertions.assertEquals(1, timetable.getTrainingSessionsForDay(DayOfWeek.MONDAY).size());
     }
 
     @Test
@@ -63,7 +99,36 @@ public class TimetableTest {
         timetable.addNewTrainingSession(singleTrainingSession);
 
         //Проверить, что за понедельник в 13:00 вернулось одно занятие
+        Assertions.assertEquals(
+                1,
+                timetable.getTrainingSessionsForDayAndTime(DayOfWeek.MONDAY, new TimeOfDay(13, 0)).size()
+        );
+
         //Проверить, что за понедельник в 14:00 не вернулось занятий
+        Assertions.assertNull(
+                timetable.getTrainingSessionsForDayAndTime(DayOfWeek.MONDAY, new TimeOfDay(14, 0))
+        );
+    }
+
+    @Test
+    void testGetTrainingSessionsForDayAndTimeMultipleSessionsAtTheSameTime() {
+        Timetable timetable = new Timetable();
+
+        Group group = new Group("Акробатика для детей", Age.CHILD, 60);
+        Coach coach = new Coach("Васильев", "Николай", "Сергеевич");
+        TrainingSession trainingSession1 = new TrainingSession(group, coach,
+                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+        TrainingSession trainingSession2 = new TrainingSession(group, coach,
+                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+
+        timetable.addNewTrainingSession(trainingSession1);
+        timetable.addNewTrainingSession(trainingSession2);
+
+        //Проверить, что за понедельник в 13:00 вернулось два занятия
+        Assertions.assertEquals(
+                2,
+                timetable.getTrainingSessionsForDayAndTime(DayOfWeek.MONDAY, new TimeOfDay(13, 0)).size()
+        );
     }
 
 }
